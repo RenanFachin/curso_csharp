d2632fca80cefb7cfba9fe2a61416549d3af94f0
Renan Fachin <renan.fachin@Hotmail.com>
2021-04-29 15:10:18 -0300

    Inicio implementacao do Tabuleiro

---
+++ Xadrez-console/tabuleiro/Cor.cs
@@ -0,0 +1,14 @@
+﻿namespace tabuleiro
+{
+    enum Cor
+    {
+        //Apenas brancas e pretas para o jogo de xadrez
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+    }
+}
