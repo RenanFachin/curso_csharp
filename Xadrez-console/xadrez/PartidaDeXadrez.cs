648b31049be04da02897c336f50ccbfbda7df70c
Renan Fachin <renan.fachin@Hotmail.com>
2021-05-06 17:14:58 -0300

    Jogada especial: promocao

--- Xadrez-console/xadrez/PartidaDeXadrez.cs
+++ Xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -141,12 +141,27 @@ namespace xadrez
 
         public void realizaJogada(Posicao origem, Posicao destino)
         {
-            Peca pecaCaputurada = executaMovimento(origem, destino);
+            Peca pecaCapturada = executaMovimento(origem, destino);
 
             if (estaEmXeque(jogadorAtual))
             {
-                desfazMovimento(origem,destino,pecaCaputurada);
+                desfazMovimento(origem,destino,pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
+            }
+
+            Peca p = tab.peca(destino);
+
+            // #Jogada especial: Promoção
+            if(p is Peao)
+            {
+                if(p.cor == Cor.Branca && destino.linha ==0 || p.cor== Cor.Preta && destino.linha == 7)
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
             }
 
 
@@ -170,7 +185,7 @@ namespace xadrez
             }
 
 
-            Peca p = tab.peca(destino);
+
             // #Jogada especial: En passant
             if(p is Peao && (destino.linha == origem.linha-2 || destino.linha == origem.linha + 2))
             {
